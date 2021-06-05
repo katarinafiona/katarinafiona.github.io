a61996614340da454b8acf28b234980658987da4
katarinafiona <79881208+katarinafiona@users.noreply.github.com>
2021-06-05 17:51:36 +0700

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,73 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+const prev = document.querySelector(".prev-sale");
+const next = document.querySelector(".next-sale");
+const carousel = document.querySelector(".carousel-container");
+const track = document.querySelector(".track");
+const cardContainer = document.querySelector(".card-container");
+let width = carousel.offsetWidth;
+const cardWidth = cardContainer.offsetWidth*5;
+let index = 0;
+
+window.addEventListener("resize", function () {
+  width = carousel.offsetWidth;
+});
+next.addEventListener("click", function (e) {
+  e.preventDefault();
+  index = index + 1;
+  prev.classList.add("show");
+  
+  if(screen.width<641){
+    track.style.transform = "translateX(" +index * -(cardContainer.offsetWidth*1.5) + "px)";
+    if (cardWidth - index * width < width) {
+      next.classList.add("hide");
+    }
+  }else{
+    track.style.transform = "translateX(" + index * -width + "px)";
+    if (track.offsetWidth - index * width < index * width) {
+      next.classList.add("hide");
+    }
+  }
+  
+}); 
+
+prev.addEventListener("click", function () {
+  index = index - 1;
+  next.classList.remove("hide");
+  if (index === 0) {
+    prev.classList.remove("show");
+  }
+  track.style.transform = "translateX(" + index * -width + "px)";
+});
+
+
